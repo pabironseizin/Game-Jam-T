ae0dc17668423b6dcb276695c1a9d9d10a0ba7ca
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2024-02-13 14:12:08 +0900

    cpp書き込み

--- Game/DriveAndAvoid/Object/Enemy_T.cpp
+++ Game/DriveAndAvoid/Object/Enemy_T.cpp
@@ -1,6 +1,14 @@
 #include "Enemy_T.h"
 #include"DxLib.h"
 
+enum Enemy
+{
+ enemy1,
+ enemy2,
+ enemy3,
+ enemy4
+};
+
 Enemy_T::Enemy_T() : Hp(0)
 {
 }
@@ -12,15 +20,47 @@ Enemy_T::~Enemy_T()
 //初期化処理
 void Enemy_T::Initialize()
 {
-	int Hp = 1000;
-
-	//出現させるX座標パターンを取得
-	float random_x = (float)(GetRand(4) * 105 + 40);
-
-	
+	Hp = 1000;
+	enemy_popcount = 0;
 }
 
 void Enemy_T::Update(float speed)
+{
+	if (enemy_popcount >= 50)
+	{
+		randomchar();
+		enemy_popcount = 0;
+	}
+	else
+	{
+		enemy_popcount++;
+	}
+}
+
+void Enemy_T::randomchar()
+{
+	int num = rand() % 100 + 1;
+
+	if (num <= 50)
+	{
+		enemy1;
+	}
+	else if (num <= 70)
+	{
+		enemy2;
+	}
+	else if (num <= 80)
+	{
+		enemy3;
+	}
+	else
+	{
+		enemy4;
+	}
+
+}
+
+void Enemy_T::Move()
 {
 	
 }
@@ -31,20 +71,14 @@ void Enemy_T::Draw() const
 }
 
 
+
 void Enemy_T::Finalize()
 {
 }
 
-void Enemy_T::randomchar()
-{
-	= rand(100) <= 50;
-	= rand(100) <= 30;
-	= rand(100) <= 20;
-	= rand(100) <= 10;
 
-}
 
 int Enemy_T::GetHp()
 {
-	
+	return Hp;
 }
--- Game/DriveAndAvoid/Object/Enemy_T.h
+++ Game/DriveAndAvoid/Object/Enemy_T.h
@@ -3,6 +3,7 @@ class Enemy_T
 {
 private:
 	int Hp;
+	int enemy_popcount;
 
 public:
 	//コンストラクタ(インスタンス生成時に最初に呼ばれる関数)
@@ -15,6 +16,7 @@ public:
 	void Update(float speed);		//更新処理
 	void Draw() const;              //描画処理
 	void Finalize();				//終了時処理
+	void Move();
 
 	int GetHp();
 	void randomchar();
